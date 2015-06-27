f8e22a1f800f545729209b1d36031d4738ce8382
Jose Eusebio Alderete <jose@Joses-MacBook-Air.local>
2015-06-27 10:46:12 -0500

    fix

--- src/main/java/com/tecsup/castell/dao/ArchivoPruebaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.tecsup.castell.dao;
-
-import com.tecsup.castell.helper.GenericCrud;
-import com.tecsup.castell.model.Usuario;
-
-public interface UsuarioDAO extends GenericCrud<Usuario>{
-    
-    Usuario findByLogin(String usuario);
-}
